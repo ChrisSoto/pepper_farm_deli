464e6e01331f03a88b049cc7771683de00db1356
ChrisSoto <christopherrsoto@gmail.com>
2024-07-22 10:32:10 -0700

    can't have two onload on the same page

--- _includes/js/home-marquee.js
+++ _includes/js/home-marquee.js
@@ -1,4 +1,21 @@
 window.onload = function() {
+
+  
+  const sticky = document.getElementById('order-now-sticky');
+  const hero = document.getElementById('order-now-hero');
+  document.addEventListener('scroll', function() {
+    const heroRect = hero.getBoundingClientRect();
+
+    if (heroRect.top <= 0) {
+      console.log('above');
+      sticky.style.display = 'block';
+    } else {
+      console.log('below');
+      sticky.style.display = 'none';
+    }
+  });
+
+
   const marquee = document.getElementById('marquee');
   const marqueeContainer = document.getElementById('marquee-container');
   let containerWidth = marqueeContainer.offsetWidth;
