4abea4d2b3b785077eac68a775d087dd5bec2bc2
chris soto <christopherrsoto@gmail.com>
2025-01-20 17:57:22 -0800

    rerun

--- .eleventy.js
+++ .eleventy.js
@@ -112,7 +112,7 @@ export default function (eleventyConfig) {
       });
   });
 
-  //
+  // rerun
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
   eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
